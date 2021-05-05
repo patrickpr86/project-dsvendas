b08addc50b6316e8b504116c0921861cd2f78843
patrickpr86 <sh4dbr@gmail.com>
2021-05-05 14:27:07 -0300

    Pagination

--- backend/src/main/java/com/patrick/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/patrick/dsvendas/services/SaleService.java
@@ -9,15 +9,20 @@ import org.springframework.transaction.annotation.Transactional;
 import com.patrick.dsvendas.entities.Sale;
 import com.patrick.dsvendas.entities.dto.SaleDTO;
 import com.patrick.dsvendas.repositories.SaleRepository;
+import com.patrick.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
 	
 	@Autowired
-	SaleRepository saleRepository;
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sale> sales = saleRepository.findAll(pageable);
 		return sales.map(x -> new SaleDTO(x));
 		
