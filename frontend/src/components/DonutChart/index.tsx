d6db03e6bc6069d4b75d4deb1776e91491a925bc
patrickpr86 <sh4dbr@gmail.com>
2021-05-08 11:59:41 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -8,35 +8,35 @@ import { BASE_URL } from 'utils/requests';
 type ChartData = {
   labels: string[],
   series: number[]
-} 
+}
 
 const DonutChart = () => {
 
-  const [chartData, setChartData] = useState<ChartData>({ labels: [], series:[] });
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
   useEffect(() => {
     axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+      .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
 
-      setChartData({ labels: myLabels, series: mySeries });          
-    });
+        setChartData({ labels: myLabels, series: mySeries });
+      });
   }, []);
-    
-    
-    const options = {
-        legend: {
-            show: true
-        }
+
+  const options = {
+    legend: {
+      show: true
     }
+  }
+
   return (
     <Chart
-    options={{...options, labels: chartData.labels}}
-    series={chartData.series}
-    type="donut"
-    height="240"
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
     />
   );
 }
